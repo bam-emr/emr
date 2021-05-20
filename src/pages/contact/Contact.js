31f8db34d0dcd151455e2d567ba130f7a8c751fc
jenniferchen11 <jennifer.chen2002@gmail.com>
2021-05-20 12:53:57 -0400

    Contact us page

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,5 +1,63 @@
-import React from "react";
+import React,{ useState, useContext } from "react";
+import {
+  Link,
+  Button,
+  TextField,
+  MenuItem,
+  Typography,
+} from "@material-ui/core";
 
 export default function Contact() {
-  return <div>Contact Page</div>;
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [message, setMessage] = useState("");
+  return( 
+    <div>
+      <h2 className="welcome">Contact Us</h2>
+      <div className = "minor-break"></div>
+      <div className = "minor-break"></div>
+      <div className = "minor-break"></div>
+      <div className = "minor-break"></div>
+      <div className = "minor-break"></div>
+
+      <div>
+        <TextField
+          required
+          label="Name"
+          value={firstName}
+          //onChange={(e) => setFirstName(e.target.value)}
+          variant="outlined"
+          color="primary"
+        />
+        <div className="line-break"></div>
+        <TextField
+          required
+          label="Email"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          variant="outlined"
+          color="primary"
+        />
+        <div className="line-break"></div>
+        <TextField
+          required
+          label="Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          variant="outlined"
+          color="primary"
+        />
+        <div className="line-break"></div>
+        <Button
+        className="field"
+        variant="contained"
+        color="primary"
+      >
+        Send Message
+      </Button>
+      </div>
+    </div>
+  
+  
+  );
 }
